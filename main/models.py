eb94015d871bf6619827e400b99db2307e69283b
farrelzzz <rajendramuhammadfarrel@gmail.com>
2025-09-08 23:16:54 +0700

    siap deploy

--- main/models.py
+++ main/models.py
@@ -1,37 +1,38 @@
 import uuid
 from django.db import models
 
-class News(models.Model):
+class Product(models.Model):
     CATEGORY_CHOICES = [
-        ('transfer', 'Transfer'),
-        ('update', 'Update'),
-        ('exclusive', 'Exclusive'),
-        ('match', 'Match'),
-        ('rumor', 'Rumor'),
-        ('analysis', 'Analysis'),
+        ('jerseys', 'Jerseys'),
+        ('shoes', 'Shoes'),
+        ('socks', 'Socks'),
+        ('balls', 'Balls'),
+        ('bags', 'Bags'),
     ]
     
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
-    thumbnail = models.URLField(blank=True, null=True)
-    product_views = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    is_featured = models.BooleanField(default=False)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # title = models.CharField(max_length=255)
+    # content = models.TextField()
+    # news_views = models.PositiveIntegerField(default=0)
+    # created_at = models.DateTimeField(auto_now_add=True)
 
     ##
     name = models.CharField(max_length=255)
     price = models.IntegerField(default=0)
     description = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+    thumbnail = models.URLField(blank=True, null=True)
+    is_iconic = models.BooleanField(default=False)
+    product_views = models.PositiveIntegerField(default=0)
+    is_featured = models.BooleanField(default=False)
     
     def __str__(self):
         return self.title
     
     @property
-    def is_news_hot(self):
-        return self.news_views > 20
+    def is_product_hot(self):
+        return self.product_views > 20
         
     def increment_views(self):
-        self.news_views += 1
+        self.product_views += 1
         self.save()
--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'npm' : '2406495653',
+        # 'npm' : '2406495653',
         'name': 'Muhammad Farrel Rajendra',
         'class': 'PBP D'
     }
